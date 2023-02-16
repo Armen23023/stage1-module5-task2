f5dd93788120d4ce487049ddd6dff28ef1040013
Armen23023 <mirzoyana805@gmail.com>
2023-02-16 19:31:05 +0400

    WIP

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,21 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class InterfaceCreator {
 
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        Operation operation = (x) -> {
+            List<Integer> list = new ArrayList<>();
+            Iterator<Integer> iterator = x.iterator();
+            while (iterator.hasNext()){
+                Integer next = iterator.next();
+                list.add(next/divider);
+            }
+            return list;
+        };
+        return operation;
     }
 }
